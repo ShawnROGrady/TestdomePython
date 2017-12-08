9597d631166186017dd2714f51bd53f4f2ad59d8
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-08 00:53:58 -0600

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,15 @@
+# Created by Shawn O'Grady on 12/7/17.
+# Copyright 2017 Shawn O'Grady. All rights reserved.
+
+ #This code is a practice Python interview question from testdome.com
+
+ #https://www.testdome.com/questions/python/palindrome/7962?visibility=1&skillId=9
+
+ # Problem statement: Write a function that checks if a given word is a palindrome. Character case should be ignored.
+ class Palindrome:
+
+    @staticmethod
+    def is_palindrome(word):
+        return None
+
+print(Palindrome.is_palindrome('Deleveled'))
