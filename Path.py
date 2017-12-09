ce4dab2bb078194013dd7335d0109357505aadb4
Shawn O'Grady <33360408+ShawnROGrady@users.noreply.github.com>
2017-12-09 15:24:50 -0600

    Implemented full functionality
    Implemented the functionality for all cases. I thought I would need to include more, since for most file systems you can represent the current directory with '.', but I guess I do not need to support this. 
    
    Passes all 4/4 tests on testdome.com

--- Path.py
+++ Path.py
@@ -14,16 +14,35 @@
         #v) The function should support both relative and absolute paths
         #vi) The function will not be passed any invalid paths
         #vii) Do not use built-in path-related functions.
+# Passes 4/4 tests
+
 class Path:
     def __init__(self, path):
         self.current_path = path
 
     def cd(self, new_path):
-        pathLength=len(new_path)
+        i=0;
         new_pathList=new_path.split('/')
-        print(pathList)
+        pathLength=len(new_pathList)
+        pathList=self.current_path.split('/')
+        #print(new_PathList)
+        if new_pathList[0]=='':
+            #direct pathname
+            del pathList[:]
+            pathList.append('/'+new_pathList[1])
+            i=i+2
+        while(i<pathLength):
+            j=len(pathList)-1
+            if new_pathList[i]=='..':
+                #parent directory
+                pathList.pop(j)
+            else:
+                pathList.append(new_pathList[i])
+            i=i+1
+        self.current_path="/".join(pathList)
+
         pass
 
 path = Path('/a/b/c/d')
-path.cd('/../x')
-print(path.current_path)
+path.cd('/x/y/../z')
+print(path.current_path) # '/x/z'
