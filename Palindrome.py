9271cee38e3e49328e63984ce29fa46093847315
lf-cont <51765212+lf-cont@users.noreply.github.com>
2019-06-21 15:10:23 -0500

    update palindrome
    improving your code

--- Palindrome.py
+++ Palindrome.py
@@ -8,16 +8,7 @@
  # Problem statement: Write a function that checks if a given word is a palindrome. Character case should be ignored.
 
  #Passes 3/3 tests
-class Palindrome:
+def Palindrome(word):
+    return word.lower()==word.lower()[::-1]
 
-    @staticmethod
-    def is_palindrome(word):
-        word=word.lower() #just setting all characters to lower case for simplicity
-        i=len(word)
-        for letter in word:
-            if letter!=word[i-1]:
-                return False
-            i=i-1
-        return True
-
-print(Palindrome.is_palindrome('Deleveled'))
+print(Palindrome('Deleveled'))
